92f0218931b7424afcc2ed9ca81abe174e0b94bc
fullmigasamurai <fullmigasamurai@users.noreply.github.com>
2020-10-17 23:26:40 -0400

    who are you response

--- lambda/index.js
+++ lambda/index.js
@@ -196,7 +196,7 @@ const QuemEsTu = {
 
 		return handlerInput.responseBuilder
 			.speak(speakOutput)
-			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+			.reprompt(speakOutput)
 			.getResponse();
 	}
 };
