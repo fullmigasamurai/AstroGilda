306408cf2c105ab5d790e894ec9b7dcf74319258
fullmigasamurai <fullmigasamurai@users.noreply.github.com>
2020-10-19 16:53:56 -0400

    getspecial

--- lambda/index.js
+++ lambda/index.js
@@ -21,7 +21,7 @@ const LaunchRequestHandler = {
 		let speakOutput = locale ==='en-US' ? "Hellow,  i am " : "olá, eu sou ";
 		// let spoken_special = JSON.parse(JSON.stringify(astro_dialogs.getDailySpecialForPeriod('sunday', 'lunch')));
 		// console.log("^^^^ - spoken special withoyt parse: " + menu.getDailySpecialForPeriod(day, period));
-		console.log("^^^^ - spoken special: " + astro_dialogs.sunday);
+		console.log("^^^^ - spoken special: " + astro_dialogs.getDailySpecialForPeriod('sunday', 'lunch'));
 		speakOutput += astroGilda;
 
 		return handlerInput.responseBuilder
