c6c97bd11ff72fe9d291ffea7a8888c7b8f20ea9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-19 22:48:09 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -31,7 +31,7 @@ const LaunchRequestHandler = {
 // 		console.log("^^^^ getAtributeFromJson: " + JSON.stringify(atribute));
 // 		console.log("^^^^ - no let stringfy: " + JSON.stringify(JSON.parse(JSON.stringify(astro_dialogs.getDailySpecialForPeriod('sunday', 'lunch')))));
 		console.log("^^^^ stringfy.pack: " + dialogs.types);
-		console.log("^^^^ dialogs: " + JSON.stringify(dialogs.types));
+		console.log("^^^^ stringfy dialogs.types: " + JSON.stringify(dialogs.types));
 		console.log("^^^^ dialogs: " + dialogs);
 		console.log("^^^^ dialogs.sunday: " + JSON.stringify(dialogs.sunday));
 // 		console.log("^^^^ dialogs.sunday.salad: " + dialogs.sunday.salad);
