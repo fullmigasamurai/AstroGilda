2461beb67bfca3e16da8122d65851f7275c9bb93
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-20 21:02:21 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -30,20 +30,20 @@ const LaunchRequestHandler = {
 	handle(handlerInput) {
 		console.log("_________________________________________________________")
 		
-	async function getName() {
-        const { serviceClientFactory, responseBuilder } = handlerInput;
-        try {
-            const upsServiceClient = serviceClientFactory.getUpsServiceClient();
-            const profileName = await upsServiceClient.getProfileName();
-            const speechResponse = `Your name is, ${profileName}`;
-            console.log("~~~~ prfile name" + profileName);
-            return profileName
-        } catch (error) {
-            console.log(JSON.stringify(error));
+    	async function getName() {
+            const { serviceClientFactory, responseBuilder } = handlerInput;
+            try {
+                const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+                const profileName = await upsServiceClient.getProfileName();
+                const speechResponse = `Your name is, ${profileName}`;
+                console.log("~~~~ prfile name" + profileName);
+                return profileName
+            } catch (error) {
+                console.log(JSON.stringify(error));
+            }
         }
-    }
-    
-    myName = getName().then();
+        
+        myName = getName();
 		
 		locale = handlerInput.requestEnvelope.request.locale;
         let speakOutput = locale ==='en-US' ? "Hellow, "+myName+"  i am " : "olá, "+myName+" eu sou ";
