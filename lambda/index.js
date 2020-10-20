ab84d6aaaaec29f35f14fc325e7418903d234250
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-20 12:31:34 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -398,7 +398,7 @@ const SessionEndedRequestHandler = {
 		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
 	},
 	handle(handlerInput) {
-		// console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+		console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
 		// Any cleanup logic goes here.
 		return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
 	}
