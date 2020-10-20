40d1a980165c600debe6237f5aa8976d7a27cd50
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-20 12:31:04 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -403,25 +403,25 @@ const SessionEndedRequestHandler = {
 		return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
 	}
 };
-/* *
-* The intent reflector is used for interaction model testing and debugging.
-* It will simply repeat the intent the user said. You can create custom handlers for your intents 
-* by defining them above, then also adding them to the request handler chain below 
-* */
-const IntentReflectorHandler = {
-	canHandle(handlerInput) {
-		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-	},
-	handle(handlerInput) {
-		const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-		const speakOutput = `You just triggered ${intentName}`;
+// /* *
+// * The intent reflector is used for interaction model testing and debugging.
+// * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+// * by defining them above, then also adding them to the request handler chain below 
+// * */
+// const IntentReflectorHandler = {
+// 	canHandle(handlerInput) {
+// 		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+// 	},
+// 	handle(handlerInput) {
+// 		const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+// 		const speakOutput = `You just triggered ${intentName}`;
 
-		return handlerInput.responseBuilder
-			.speak(speakOutput)
-			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-			.getResponse();
-	}
-};
+// 		return handlerInput.responseBuilder
+// 			.speak(speakOutput)
+// 			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+// 			.getResponse();
+// 	}
+// };
 /**
  * Generic error handling to capture any syntax or routing errors. If you receive an error
  * stating the request handler chain is not found, you have not implemented a handler for
