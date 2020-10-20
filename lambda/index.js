9364c9b2b6997e5ddea2218b7500ad099e4b3f03
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-20 15:43:06 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -78,7 +78,7 @@ const ChameAstrogilda = {
 		const { serviceClientFactory, responseBuilder } = handlerInput;
 		
 		try {
-            // const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+            const upsServiceClient = serviceClientFactory.getUpsServiceClient();
             // const profileName = await upsServiceClient.getProfileName();
             // const speechResponse = `Your name is, ${profileName}`;
             const speechResponse = `Your name is,`;
