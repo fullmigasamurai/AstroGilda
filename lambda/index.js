41ec2ce650ce5428c1b43225303ff3e4101e2817
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-20 20:59:55 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -57,36 +57,6 @@ const LaunchRequestHandler = {
 	}
 };
 
-const GreetMeIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'GreetMeIntent';
-  },
-  async handle(handlerInput) {
-    const { serviceClientFactory, responseBuilder } = handlerInput;
-    try {
-      const upsServiceClient = serviceClientFactory.getUpsServiceClient();
-      const profileName = await upsServiceClient.getProfileName();
-      const speechResponse = `Your name is, ${profileName}`;
-      return responseBuilder
-                      .speak(speechResponse)
-                      .withSimpleCard(APP_NAME, speechResponse)
-                      .getResponse();
-    } catch (error) {
-      console.log(JSON.stringify(error));
-      if (error.statusCode == 403) {
-        return responseBuilder
-        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
-        .withAskForPermissionsConsentCard([FULL_NAME_PERMISSION])
-        .getResponse();
-      }
-      console.log(JSON.stringify(error));
-      const response = responseBuilder.speak(messages.ERROR).getResponse();
-      return response;
-    }
-  },
-}
-
 const ChameAstrogilda = {
 	canHandle(handlerInput) {
 		return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
